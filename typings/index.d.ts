188da4b2671f9593b90ef6bd210d4048a439b8dc
Raghav Misra <raghav.m2014@gmail.com>
2021-10-01 12:00:13 -0700

    Write typings

---
+++ typings/index.d.ts
@@ -0,0 +1,33 @@
+declare namespace FourTwenty {
+    type CanBePromise<T> = T | Promise<T>;
+
+    type PropType = object | null;
+
+    type ChildWrapper = () => RenderableResult;
+
+    type RenderableResult = CanBePromise<string | IElement>;
+
+    interface IElement {
+        tag: string;
+        props: PropType;
+        children: ChildWrapper[];
+    }
+
+    type Component<Props extends PropType = {}> =
+        (props: Props, children: RenderableResult[]) =>
+            RenderableResult | RenderableResult[];
+}
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            [tag: string]: any;
+        }
+    
+        type Element = FourTwenty.IElement;
+    
+        type Component<Props extends FourTwenty.PropType = {}> = FourTwenty.Component<Props>;
+    }
+}
+
+export = FourTwenty;
